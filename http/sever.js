58a101209ff0a9105d4850381d8ea60ed9b53d46
高婷婷 <gaotingting@gaotingtingdeMacBook-Air.local>
2020-03-10 16:49:25 +0800

    ’update‘

--- http/sever.js
+++ http/sever.js
@@ -1,19 +1,19 @@
 
-const querystring = require('querystring');
+//sever
+
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-
-    const data=[
-        {index:0,context:"hello 0"},
-        {index:1,context:"hello 1"},
-        {index:2,context:"hello 2"},
-    ];
+const data=[
+    {index:0,context:"hello 0"},
+    {index:1,context:"hello 1"},
+    {index:2,context:"hello 2"},
+];
 
 let server = http.createServer(function (request, response) {
     const parsedUrl = url.parse(request.url);
     if (parsedUrl.pathname === '/favicon.ico') {
-        // 如果请求url是浏览器icon，比如 http://localhost:3000/favicon.ico的情况
+        // 如果请求url是浏览器icon
         // 就返回一个200就好了
         response.writeHead(200);
         response.end();
@@ -25,7 +25,7 @@ let server = http.createServer(function (request, response) {
         response.end(JSON.stringify(data));
         return;
     }
-    // 如果访问的是根路径，则把游戏页面读出来返回出去
+    // 如果访问的是根路径，返回页面
     if (parsedUrl.pathname === '/') {
         fs.createReadStream(__dirname + '/index.html').pipe(response);
     }
